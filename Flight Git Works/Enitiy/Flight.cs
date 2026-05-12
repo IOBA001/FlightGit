413547e157756f2e36f15c3279a39ad600720207
IOBA001 <nklv717@gmail.com>
2026-05-12 22:20:25 +0400

    Add Entity folder

---
+++ Flight Git Works/Enitiy/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace Flight_Git_Works.Enitiy
+{
+    public class Flight
+    {
+        public string Id { get; set; }
+        public string FlightNumber { get; set; }
+        public string DepartureCity { get; set; }
+        public string ArrivalCity { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int AvailableSeats { get; set; }
+    }
+}
