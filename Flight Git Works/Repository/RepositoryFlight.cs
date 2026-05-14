6d0b754d2e1383042b489472d2ab2fde7b4f902d
IOBA001 <nklv717@gmail.com>
2026-05-14 22:52:40 +0400

    Create Flight

--- Flight Git Works/Repository/RepositoryFlight.cs
+++ Flight Git Works/Repository/RepositoryFlight.cs
@@ -1,16 +1,38 @@
-﻿using Flight_Git_Works.Enitiy;
+﻿using Dapper;
+using Flight_Git_Works.Enitiy;
 using Flight_Git_Works.IRepository;
+using System.Data;
 
 namespace Flight_Git_Works.Repository
 {
     public class RepositoryFlight : IRepositoryFlight
     {
-        public bool BookTicket(Flight flight, string passengerName)
+        private readonly IDbConnection _dbConnection;
+        public RepositoryFlight(IDbConnection dbConnection) 
+        {
+            _dbConnection = dbConnection;
+        }
+        public async Task<Flight> AddFlight(Flight flight)
+        {
+            var addflight = await _dbConnection.ExecuteAsync("CreateFlight", new
+            {
+                Id = flight.Id,
+                FlightNumber = flight.FlightNumber,
+                DepartureCity = flight.DepartureCity,
+                ArrivalCity = flight.ArrivalCity,
+                DepartureTime = flight.DepartureTime,
+                AvailableSeats = flight.AvailableSeats,
+            },
+                 commandType: CommandType.StoredProcedure);
+            return addflight > 0 ? flight : null;
+        }
+
+        Task IRepositoryFlight.DeleteFlight(int Id)
         {
             throw new NotImplementedException();
         }
 
-        public bool SearchFlights(string departureCity, string arrivalCity, DateTime departureDate)
+        Task<List<Flight>> IRepositoryFlight.GetAllFlights()
         {
             throw new NotImplementedException();
         }
