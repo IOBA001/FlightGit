944cf601ca623a03fe475d638eea107daa64f6a7
IOBA001 <nklv717@gmail.com>
2026-05-14 22:58:02 +0400

    change

--- Flight Git Works/Enitiy/Reservation.cs
+++ Flight Git Works/Enitiy/Reservation.cs
@@ -2,6 +2,7 @@
 {
     public class Reservation
     {
+        public int Id { get; set; }
         public string ReservationNumber { get; set; }
         public string PassengerName { get; set; }
         public Flight Flight { get; set; }
